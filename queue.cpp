e9c7d41dcc85a87bb12cdf7538b45e70d94bd93d
Farhan200222 <m.anasfarhan02@gmail.com>
2025-01-10 19:17:27 +0800

    queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,73 +1,201 @@
 #include <iostream>
-#include <queue>
 #include <string>
 using namespace std;
 
-int main() {
+// Queue Class Definition
+class Queue
+{
+public:
+    Queue(int QueueSize);
+    ~Queue();
+    void Insert(string Value);
+    string Remove();
+    string First();
+    void Display();
+
+private:
+    string* Array;
+    int Front;
+    int Rear;
+    int Count;
+    int ArraySize;
+};
+
+// Queue Implementation
+Queue::Queue(int QueueSize)
+{
+    Array = new string[QueueSize];
+    ArraySize = QueueSize;
+    Front = 0;
+    Rear = ArraySize - 1;
+    Count = 0;
+}
+
+Queue::~Queue()
+{
+    delete[] Array;
+}
+
+void Queue::Insert(string Value)
+{
+    if (Count >= ArraySize)
+    {
+        cout << "Waiting list is FULL!\n";
+        return;
+    }
+    Rear = (Rear + 1) % ArraySize;
+    Array[Rear] = Value;
+    Count++;
+}
+
+string Queue::Remove()
+{
+    if (Count == 0)
+    {
+        cout << "Waiting list is empty!\n";
+        return "";
+    }
+    string Temp = Array[Front];
+    Front = (Front + 1) % ArraySize;
+    Count--;
+    return Temp;
+}
+
+string Queue::First()
+{
+    if (Count == 0)
+    {
+        return "";
+    }
+    return Array[Front];
+}
+
+void Queue::Display()
+{
+    if (Count == 0)
+    {
+        cout << "Waiting list is empty!\n";
+        return;
+    }
+    int index = Front;
+    for (int i = 0; i < Count; i++)
+    {
+        cout << Array[index] << endl;
+        index = (index + 1) % ArraySize;
+    }
+}
+
+// EventManagement Implementation Continues
+class EventManagement
+{
+public:
+    EventManagement(int maxCapacity, int waitingListSize);
+    ~EventManagement();
+    void RegisterParticipant(string name);
+    void CancelRegistration(string name);
+    void DisplayParticipants();
+    void DisplayWaitingList();
+
+private:
     int maxCapacity;
-    cout << "Enter event capacity: ";
-    cin >> maxCapacity;
+    string* participants;
+    int participantCount;
+    Queue waitingList;
+};
 
-    queue<string> registered, waitingList;
-    int choice;
-    string name;
+EventManagement::EventManagement(int maxCapacity, int waitingListSize)
+    : maxCapacity(maxCapacity), waitingList(waitingListSize)
+{
+    participants = new string[maxCapacity];
+    participantCount = 0;
+}
 
-    while (true) {
-        cout << "\n1. Register Participant\n2. Cancel Registration\n3. View Registered\n4. View Waiting List\n5. Exit\nChoice: ";
-        cin >> choice;
-        cin.ignore();
+EventManagement::~EventManagement()
+{
+    delete[] participants;
+}
 
-        if (choice == 1) {
-            cout << "Enter name: ";
-            getline(cin, name);
-            if (registered.size() < maxCapacity) {
-                registered.push(name);
-                cout << name << " registered successfully!\n";
-            } else {
-                waitingList.push(name);
-                cout << "Event full. " << name << " added to waiting list.\n";
-            }
-        } else if (choice == 2) {
-            if (registered.empty()) {
-                cout << "No registered participants to cancel.\n";
-            } else {
-                cout << "Cancelling registration for " << registered.front() << ".\n";
-                registered.pop();
-                if (!waitingList.empty()) {
-                    registered.push(waitingList.front());
-                    cout << waitingList.front() << " moved to registered list.\n";
-                    waitingList.pop();
-                }
-            }
-        } else if (choice == 3) {
-            if (registered.empty()) {
-                cout << "No registered participants.\n";
-            } else {
-                cout << "Registered Participants:\n";
-                queue<string> temp = registered;
-                while (!temp.empty()) {
-                    cout << temp.front() << endl;
-                    temp.pop();
-                }
-            }
-        } else if (choice == 4) {
-            if (waitingList.empty()) {
-                cout << "No participants in waiting list.\n";
-            } else {
-                cout << "Waiting List:\n";
-                queue<string> temp = waitingList;
-                while (!temp.empty()) {
-                    cout << temp.front() << endl;
-                    temp.pop();
-                }
-            }
-        } else if (choice == 5) {
-            cout << "Exiting.\n";
+void EventManagement::RegisterParticipant(string name)
+{
+    if (participantCount >= maxCapacity)
+    {
+        cout << name << " added to the waiting list.\n";
+        waitingList.Insert(name);
+    }
+    else
+    {
+        participants[participantCount++] = name;
+        cout << name << " successfully registered.\n";
+    }
+}
+
+void EventManagement::CancelRegistration(string name)
+{
+    int index = -1;
+    for (int i = 0; i < participantCount; i++)
+    {
+        if (participants[i] == name)
+        {
+            index = i;
             break;
-        } else {
-            cout << "Invalid choice.\n";
         }
     }
+
+    if (index != -1)
+    {
+        for (int i = index; i < participantCount - 1; i++)
+        {
+            participants[i] = participants[i + 1];
+        }
+        participantCount--;
+        cout << name << " has been removed from the participants list.\n";
+
+        if (waitingList.First() != "")
+        {
+            string nextParticipant = waitingList.Remove();
+            participants[participantCount++] = nextParticipant;
+            cout << nextParticipant << " has been moved from the waiting list to the participants list.\n";
+        }
+    }
+    else
+    {
+        cout << name << " is not registered.\n";
+    }
+}
+
+void EventManagement::DisplayParticipants()
+{
+    cout << "Participants list:" << endl;
+    for (int i = 0; i < participantCount; i++)
+    {
+        cout << participants[i] << endl;
+    }
+}
+
+void EventManagement::DisplayWaitingList()
+{
+    cout << "Waiting list:" << endl;
+    waitingList.Display();
+}
+
+int main()
+{
+    EventManagement event(3, 5);
+
+    event.RegisterParticipant("Alice");
+    event.RegisterParticipant("Bob");
+    event.RegisterParticipant("Charlie");
+    event.RegisterParticipant("David");
+    event.RegisterParticipant("Eve");
+
+    event.DisplayParticipants();
+    event.DisplayWaitingList();
+
+    cout << endl;
+    event.CancelRegistration("Bob");
+
+    event.DisplayParticipants();
+    event.DisplayWaitingList();
 
     return 0;
 }
